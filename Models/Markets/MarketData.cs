1001ac73119b8ea4fef0fd09538d0598c0a4a778
Alex Olkhovyk <Alex Olkhovyk@DESKTOP-T1AC3NI>
2024-06-27 10:48:39 +0300

    Created market models

---
+++ Models/Markets/MarketData.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinCapDesktop.Models.Markets
+{
+    internal class MarketData : ViewModels.Base.ViewModel
+    {
+        [JsonProperty("exchangeId")]
+        public string ExchangeId { get; set; }
+
+        [JsonProperty("baseSymbol")]
+        public string BaseSymbol { get; set; }
+
+        [JsonProperty("baseId")]
+        public string BaseId { get; set; }
+
+        [JsonProperty("quoteSymbol")]
+        public string QuoteSymbol { get; set; }
+
+        [JsonProperty("quoteId")]
+        public string QuoteId { get; set; }
+
+        [JsonProperty("priceUsd")]
+        public double? PriceUsd { get; set; }
+
+        [JsonProperty("volumeUsd24Hr")]
+        public double? VolumeUsd24Hr { get; set; }
+    }
+}
---
+++ Models/Markets/MarketsCollection.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace CoinCapDesktop.Models.Markets
+{
+    internal class MarketsCollection : ViewModels.Base.ViewModel
+    {
+        [JsonProperty("data")]
+        public ObservableCollection<MarketData> Data { get; set; }
+
+        [JsonProperty("timestamp")]
+        public long Timestamp { get; set; }
+    }
+}

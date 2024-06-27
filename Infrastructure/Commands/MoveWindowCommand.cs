0a38829a0b87ed7d2c8f95b32f83738be3c934a9
Alex Olkhovyk <Alex Olkhovyk@DESKTOP-T1AC3NI>
2024-06-27 17:01:52 +0300

    Added commands for closing, moving and minimizing window

---
+++ Infrastructure/Commands/MoveWindowCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace CoinCapDesktop.Infrastructure.Commands
+{
+    internal class MoveWindowCommand : Command
+    {
+        public override bool CanExecute(object parameter)
+         => true;
+        public override void Execute(object parameter)
+        {
+            Application.Current.MainWindow.DragMove();
+        }
+    }
+}
